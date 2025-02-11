cc40b04e3aa83f0e5b159eafa35a7bdb38a22e83
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2025-02-11 11:21:05 -0600

    feat: create cron ticker app

---
+++ cron-ticker/app.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+    times++;
+    console.log('running a task every 5 seconds', times);
+});
+
+console.log('Cron job started');
